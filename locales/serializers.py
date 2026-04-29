33d3e058e40e6c78f15fab3d0a71288b778d07cf
svelasco-and <svelasco@telynet.com>
2026-04-29 16:03:24 +0200

    update serializer

--- locales/serializers.py
+++ locales/serializers.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
-
 from .models import Local
 
 class LocalSerializer(serializers.ModelSerializer):
+    imagen = serializers.SerializerMethodField()
+
     class Meta:
         model = Local
-        fields = [ 
-                    'nombre',
-                    'ubicacion',
-                    'telefono',
-                    'web',
-                    'imagen',
-                    'estado' ]
+        fields = [
+            'nombre',
+            'ubicacion',
+            'telefono',
+            'web',
+            'imagen',
+            'estado'
+        ]
 
+    def get_imagen(self, obj):
+        if obj.imagen:
+            return obj.imagen.url
+        return None
